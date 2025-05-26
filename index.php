ec74f9128d8ab7c9567c8940c23393fd856f10ab
KATHIR A/L RUTHIRAPATHY <146920555+kathirpathy@users.noreply.github.com>
2025-05-26 15:08:40 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Sistem Permohonan Cuti ILP</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container text-center mt-5">
+        <h1>Sistem Permohonan Cuti ILP Kuala Langat</h1>
+        <p>Siapakah anda?</p>
+        <a href="loginPelajar.php" class="btn btn-primary">Pelajar</a>
+        <a href="loginKb.php" class="btn btn-primary">Ketua Bahagian</a>
+        <a href="loginBppl.php" class="btn btn-primary">BPPL</a>
+    </div>
+</body>
+</html>
